a2e0618b43df143562ae02c05a750aae8453dfa2
saburday <yteslenko@yahoo.com>
2017-10-02 12:33:52 +0300

    Filter

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+    mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(reg,res){
+      var query = reg.query;
+        Book.find(query, function(err,books){
+          if(err)
+             res.status(500).send(err)
+          else
+             res.json(books);
+        });
+    });
+
+app.use('/api', bookRouter);
+
+app.get('/', function(reg, res){
+  res.send('welcome to my API');
+});
+
+app.listen(port, function(){
+  console.log('Running on PORT: '+ port);
+});
